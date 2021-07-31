2ecf0665b56c01deb7e62e7b40740e556da063c8
Hiroki Tominaga <hirokitominaga@Hirokis-MacBook-Pro.local>
2021-08-01 02:08:37 +0900

    sql test: destroy table instead of sync

--- libs/db/src/lib/db.spec.ts
+++ libs/db/src/lib/db.spec.ts
@@ -9,7 +9,8 @@ beforeAll(async () => {
 
 describe('User', () => {
   beforeEach(async () => {
-    await User.sync({ force: true }) // drops table and re-creates it
+    // await User.truncate({ force: true }) // drops table and re-creates it
+    await User.destroy({ where: {}, force: true }) // delete all data from table.
   })
   it('should add a user', async () => {
     const id = uuidv4()
@@ -21,7 +22,7 @@ describe('User', () => {
     })
     await user.save()
 
-    const users = await User.findAll()
+    const users = await User.findAll({ where: { id } })
     expect(users.length).toBe(1)
   })
 })
