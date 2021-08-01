816c2bd3630f2252e6fbefc4c529067e95178b5b
Hiroki Tominaga <hirokitominaga@Hirokis-MacBook-Pro.local>
2021-08-01 21:14:46 +0900

    added reference

--- libs/db-interfaces/src/lib/repositories/baseRepo.ts
+++ libs/db-interfaces/src/lib/repositories/baseRepo.ts
@@ -1,3 +1,6 @@
+// 【参照】
+// Title: Implementing DTOs, Mappers & the Repository Pattern using the Sequelize ORM [with Examples] - DDD w/ TypeScript
+// URL: https://khalilstemmler.com/articles/typescript-domain-driven-design/repository-dto-mapper/#Data-Mappers
 export interface Repo<Attributes, CreateAttributes, UpdationAttributes> {
   getById(id: string): Promise<Attributes | void>
   create(data: CreateAttributes): Promise<void>
