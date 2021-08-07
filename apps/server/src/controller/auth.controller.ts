f8545fd6cc2e98f0239ab808ed1db01a84ac133f
Hiroki Tominaga <hirokitominaga@Hirokis-MacBook-Pro.local>
2021-08-07 22:40:48 +0900

    login logic into AuthService

--- apps/server/src/controller/auth.controller.ts
+++ apps/server/src/controller/auth.controller.ts
@@ -1,14 +1,6 @@
 import { RequestHandler } from 'express'
 import { UserAttributes } from '@jira-clone/shared-types'
-import { UserRepo } from '@jira-clone/apps/server/repositories'
-import { StrUtils } from '../util/stringUtils'
 import { authUtils } from '../util/authUtils'
-import {
-  HTTPAlreadyExistsError,
-  HTTPInternalServerError,
-  HTTPNotFoundError,
-  HTTPUnauthorizedError,
-} from '../valueObject/httpError'
 import { AuthService } from '../services/auth.service'
 
 export const registerUser: RequestHandler<
@@ -18,7 +10,6 @@ export const registerUser: RequestHandler<
 > = async (req, res, next) => {
   try {
     const { name, email, password } = req.body
-
     const { token, user } = await AuthService.register({
       name,
       email,
@@ -26,7 +17,7 @@ export const registerUser: RequestHandler<
     })
 
     res.setHeader('set-cookie', [authUtils.createCookie(token)])
-    res.status(201).send({ user })
+    return res.status(201).send({ user })
   } catch (e) {
     next(e)
   }
@@ -36,25 +27,14 @@ export const login: RequestHandler<
   {},
   { user: UserAttributes },
   { email: string; password: string }
-> = async (req, res) => {
-  const { email, password } = req.body
-  const user = await UserRepo.getWithHashedPasswordByEmail(email)
-  if (!user) {
-    throw new HTTPNotFoundError('Not found user with the account.')
+> = async (req, res, next) => {
+  try {
+    const { email, password } = req.body
+    const { user, token } = await AuthService.login({ email, password })
+
+    res.setHeader('set-cookie', [authUtils.createCookie(token)])
+    return res.status(200).send({ user })
+  } catch (e) {
+    next(e)
   }
-
-  const isValidPassword = authUtils.isPasswordMaching(
-    password,
-    user.password_hash,
-  )
-  if (!isValidPassword) {
-    throw new HTTPUnauthorizedError('password is wrong')
-  }
-
-  // set-token in cookie;
-  const token = authUtils.createToken(user.id)
-  res.setHeader('set-cookie', [authUtils.createCookie(token)])
-
-  delete user.password_hash
-  res.status(200).send({ user })
 }
--- apps/server/src/services/auth.service.ts
+++ apps/server/src/services/auth.service.ts
@@ -5,14 +5,20 @@ import { StrUtils } from '../util/stringUtils'
 import {
   HTTPAlreadyExistsError,
   HTTPInternalServerError,
+  HTTPNotFoundError,
+  HTTPUnauthorizedError,
 } from '../valueObject/httpError'
 
 interface IAuthService {
-  register({
-    name,
-    email,
-    password,
+  register(data: {
+    name: string
+    email: string
+    password: string
   }): Promise<{ token: TokenData; user: UserAttributes }>
+  login(data: {
+    email: string
+    password: string
+  }): Promise<{ user: UserAttributes; token: TokenData }>
 }
 
 class AuthServiceImpl implements IAuthService {
@@ -21,9 +27,9 @@ class AuthServiceImpl implements IAuthService {
     email,
     password,
   }: {
-    name: any
-    email: any
-    password: any
+    name: string
+    email: string
+    password: string
   }): Promise<{ token: TokenData; user: UserAttributes }> {
     const alreadyRegisteredUser = await UserRepo.getByEmail(email)
     if (alreadyRegisteredUser) {
@@ -48,6 +54,32 @@ class AuthServiceImpl implements IAuthService {
 
     const token = authUtils.createToken(user.id)
 
+    return { user, token }
+  }
+  async login({
+    email,
+    password,
+  }: {
+    email: string
+    password: string
+  }): Promise<{ user: UserAttributes; token: TokenData }> {
+    const user = await UserRepo.getWithHashedPasswordByEmail(email)
+    if (!user) {
+      throw new HTTPNotFoundError('Not found user with the account.')
+    }
+
+    const isValidPassword = authUtils.isPasswordMaching(
+      password,
+      user.password_hash,
+    )
+    if (!isValidPassword) {
+      throw new HTTPUnauthorizedError('password is wrong')
+    }
+
+    // set-token in cookie;
+    const token = authUtils.createToken(user.id)
+
+    delete user.password_hash
     return { user, token }
   }
 }
